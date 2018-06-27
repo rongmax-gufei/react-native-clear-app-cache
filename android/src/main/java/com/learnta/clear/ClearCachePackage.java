8379657068b06ce388890954e6d36e6a3ce27ecb
gufei <gufei@learnta.com>
2018-06-27 10:28:11 +0800

    Android去除旧版本的修饰符

--- android/src/main/java/com/learnta/clear/ClearCachePackage.java
+++ android/src/main/java/com/learnta/clear/ClearCachePackage.java
@@ -19,7 +19,6 @@ public class ClearCachePackage implements ReactPackage {
         return modules;
     }
 
-    @Override
     public List<Class<? extends JavaScriptModule>> createJSModules() {
         return Collections.emptyList();
     }
